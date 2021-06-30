bfceb42ca6753b5bab6ae5db36c7bde458c2190a
ebcms <56634967+ebcms@users.noreply.github.com>
2021-06-30 15:44:04 +0800

    Update index.php

--- src/template/default/index.php
+++ src/template/default/index.php
@@ -89,40 +89,42 @@
         }
     }
     $(function() {
-        $('[data-toggle="tooltip"]').tooltip();
+        $('[data-bs-toggle="tooltip"]').tooltip();
     });
 </script>
 <script src="https://cdn.jsdelivr.net/npm/holderjs@2.9.6/holder.min.js" integrity="sha256-yF/YjmNnXHBdym5nuQyBNU62sCUN9Hx5awMkApzhZR0=" crossorigin="anonymous"></script>
-<div>
+<div class="row row-cols-auto gy-4">
     {foreach $plugins as $name => $vo}
-    <div class="d-flex mb-2 p-1">
-        <div class="me-2">
-            {if isset($vo['logo']) && $vo['logo']}
-            <img style="cursor:pointer;height:80px;width:80px;" class="img-thumbnail img-fluid mr-3" src="{$vo.logo}">
-            {else}
-            <img style="cursor:pointer;height:80px;width:80px;" class="img-thumbnail img-fluid mr-3" data-src="holder.js/80x80?auto=yes&text=nopic&size=16">
-            {/if}
-        </div>
-        <div class="flex-fill">
-            <div class="mt-0 mb-1"><strong>{$vo['title']??$name}</strong></div>
-            <div class="text-muted">{$vo['description']??'暂无介绍'}</div>
-            <div>
-                {if $vo['_install']}
-                {if $vo['_disabled']}
-                <span class="badge bg-warning" style="cursor:pointer;" onclick="change('{$name}', 0);" data-toggle="tooltip" title="应用已停用，点此切换">未启用...</span>
-                <span class="badge bg-secondary" style="cursor:pointer;" onclick="uninstall('{$name}');" data-toggle="tooltip" title="点此卸载此插件">卸载</span>
+    <div class="col">
+        <div class="d-flex" style="width: 350px;">
+            <div class="me-3">
+                {if isset($vo['logo']) && $vo['logo']}
+                <img style="cursor:pointer;height:100px;width:100px;" class="img-thumbnail img-fluid" src="{$vo.logo}">
                 {else}
-                <span class="badge bg-success" style="cursor:pointer;" onclick="change('{$name}', 1);" data-toggle="tooltip" title="应用运行中，点此切换">运行中...</span>
-                {if $vo['manager-url']}
-                <a class="badge bg-primary" href="{:$router->buildUrl(...$vo['manager-url'])}">管理</a>
+                <img style="cursor:pointer;height:100px;width:100px;" class="img-thumbnail img-fluid" data-src="holder.js/80x80?auto=yes&text=nopic&size=16">
                 {/if}
-                {/if}
-                {else}
-                <span class="badge bg-danger" style="cursor:pointer;" onclick="install('{$name}');" data-toggle="tooltip" title="该插件未安装，点此安装">安装</span>
-                <span class="badge bg-secondary" style="cursor:pointer;" onclick="del('{$name}');" data-toggle="tooltip" title="彻底删除该插件">删除</span>
-                {/if}
-                {php}$url=$router->buildUrl('/ebcms/plugin/info',['name'=>$name]);{/php}
-                <span class="badge bg-primary" style="cursor:pointer;" onclick="javascript:M.open({url:'{$url}', title:'详情'});">详情</span>
+            </div>
+            <div class="flex-fill position-relative">
+                <div class="mt-0 mb-2"><strong>{$vo['title']??$name}</strong></div>
+                <div class="text-muted">{$vo['description']??'暂无介绍'}</div>
+                <div class="position-absolute bottom-0 start-0">
+                    {if $vo['_install']}
+                    {if $vo['_disabled']}
+                    <a class="btn btn-sm btn-outline-secondary" style="cursor:pointer;" href="javascript:change('{$name}', 0);" data-bs-toggle="tooltip" title="应用已停用，点此切换">未运行...</a>
+                    <a class="btn btn-sm btn-outline-secondary" style="cursor:pointer;" href="javascript:uninstall('{$name}');" data-bs-toggle="tooltip" title="点此卸载此插件">卸载</a>
+                    {else}
+                    <a class="btn btn-sm btn-outline-success" style="cursor:pointer;" href="javascript:change('{$name}', 1);" data-bs-toggle="tooltip" title="应用运行中，点此切换">运行中...</a>
+                    {if $vo['manager-url']}
+                    <a class="btn btn-sm btn-outline-primary" href="{:$router->buildUrl(...$vo['manager-url'])}">管理</a>
+                    {/if}
+                    {/if}
+                    {else}
+                    <a class="btn btn-sm btn-outline-danger" style="cursor:pointer;" href="javascript:install('{$name}');" data-bs-toggle="tooltip" title="该插件未安装，点此安装">安装</a>
+                    <a class="btn btn-sm btn-outline-secondary" style="cursor:pointer;" href="javascript:del('{$name}');" data-bs-toggle="tooltip" title="彻底删除该插件">删除</a>
+                    {/if}
+                    {php}$url=$router->buildUrl('/ebcms/plugin/info',['name'=>$name]);{/php}
+                    <a class="btn btn-sm btn-outline-primary" style="cursor:pointer;" href="javascript:M.open({url:'{$url}', title:'详情'});">详情</a>
+                </div>
             </div>
         </div>
     </div>
